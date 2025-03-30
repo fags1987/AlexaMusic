1c194b51cde807475cee0d095dff4e30b5b50e26
fags1987 <110860429+fags1987@users.noreply.github.com>
2025-03-30 15:42:18 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -108,7 +108,7 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/d593c6064ff7657d0c714.jpg"
+    "START_IMG_URL", "https://ibb.co/cKhHrSr8"
 )
 
 PING_IMG_URL = getenv(
